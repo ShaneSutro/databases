7efc1b78a7b1e78f8893e415c6eee4db8a2fa01d
Shane Sutro <shanesutro@gmail.com>
2021-01-12 21:07:51 -0700

    End of night Tuesday

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,13 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      db.connect((err) => {
+        if (err) { throw err; }
+        console.log('connected to database');
+      });
+
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
